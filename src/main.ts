3f84fef1382cc1783fef3c317de1dfe73586580b
Mario-Duarte <djtsunamimario@gmail.com>
2025-08-05 23:53:56 +0100

    updated test page

--- src/main.ts
+++ src/main.ts
@@ -12,21 +12,28 @@ const app = createApp({
       <VueImageZooom 
         src="https://images.pexels.com/photos/32357295/pexels-photo-32357295.jpeg" 
         alt="Demo image" 
-        width="300"
-        height="200"
+        :width="300"
+        :height="200"
         zoom="250%"
       />
       <VueImageZooom 
         src="https://images.pexsels.com/photos/32357295/pexels-photo-32357295.jpeg" 
         alt="Demo image error" 
-        width="300"
-        height="200"
+        :width="300"
+        :height="200"
         zoom="250%"
       />
       <VueImageZooom 
         src="https://images.pexels.com/photos/32357295/pexels-photo-32357295.jpeg" 
         alt="Demo image full width" 
         fullWidth
+      />
+      <VueImageZooom 
+        src="https://images.pexsels.com/photos/32357295/pexels-photo-32357295.jpeg" 
+        alt="Demo image custom error" 
+        fullWidth
+        :onErrorCallback="(error) => console.error('Image load error:', error)"
+        :errorMessage="'Custom error message: Image failed to load'"
       />
       </div>
     </div>
