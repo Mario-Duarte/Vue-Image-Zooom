09742ea75ae958e94ee7c0e59823809fd6638e7a
Mario-Duarte <djtsunamimario@gmail.com>
2025-08-07 23:26:21 +0100

    changed config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/vue-image-zooom/',
-  plugins: [
-    vue(),
-    tailwindcss(),
-  ],
+  base: "",
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      vue: 'vue/dist/vue.esm-bundler.js'
-    }
+      vue: "vue/dist/vue.esm-bundler.js",
+    },
   },
   build: {
-    outDir: 'docs',
+    outDir: "docs",
   },
 });
