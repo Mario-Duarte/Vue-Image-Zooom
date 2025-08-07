708f88f10d46cbae35f46b0b85a65404a7eef326
Mario-Duarte <djtsunamimario@gmail.com>
2025-08-07 23:21:17 +0100

    Adding tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
   base: '/vue-image-zooom/',
   plugins: [
-    vue()
+    vue(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
